4672146b7f0361c447639f7b5c40b7babda99cd0
DecisionDisorder <guswhd990507@naver.com>
2022-05-24 17:38:21 +0900

    220524-firebaseParser_get

--- app/src/main/java/com/sweteam5/ladybugadmin/FirebaseParser.java
+++ app/src/main/java/com/sweteam5/ladybugadmin/FirebaseParser.java
@@ -5,13 +5,14 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.firestore.DocumentSnapshot;
 
 public class FirebaseParser<T> {
 
     FirebaseFirestore fsdb= FirebaseFirestore.getInstance();
     DatabaseReference fbdb;
 
-    public boolean sendToFireBase (String collectionPath, String DocumentID, T content) {
+    public boolean setDataOnFireBase (String collectionPath, String DocumentID, T content) {
         ResultReturn resultReturn = new ResultReturn();
         fsdb.collection(collectionPath).document(DocumentID).set(content).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
@@ -28,7 +29,25 @@ public class FirebaseParser<T> {
         return resultReturn.result;
     }
 
+    public T getFromFireBase (String collectionPath, String documentID, String equal, T content) {
+        ResultReturn resultReturn = new ResultReturn();
+        fsdb.collection("notice").document(documentID).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot snapshot) {
+                resultReturn.resultObject = (T) snapshot.toObject(content.getClass());
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                resultReturn.resultObject = null;
+            }
+        });
+        return resultReturn.resultObject;
+    }
+
     class ResultReturn {
         public boolean result;
+        public T resultObject;
     }
 }
